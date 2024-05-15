b8bec7756e02ea8fc1bf6104a83722d22fe7ae50
Kawatan1927 <solxyz1927@gmail.com>
2024-05-15 16:46:13 +0900

    [add]ログインチェック機能追加
    CheckLoginAspect.javaによるログインチェック機能を追加しました。

---
+++ src/main/java/com/example/demo/aop/CheckLoginAspect.java
@@ -0,0 +1,43 @@
+package com.example.demo.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Account;
+
+@Aspect
+@Component
+public class CheckLoginAspect {
+	
+	@Autowired
+	Account account;
+	
+	@Before("execution(* com.example.demo.controller.*Controller.*(..))")
+	public void writelog(JoinPoint jp) {
+		if (account == null || account.getUserName() == null || account.getUserPassword().length() == 0) {
+			System.out.print("ゲスト");
+		}else {
+			System.out.print(account.getUserName() + ":");
+		}
+		
+		System.out.println(jp.getSignature());
+	}
+	
+	@Around("execution(* com.example.demo.controller.ItemController.*(..)) ||"
+			+ "execution(* com.example.demo.controller.CartController.*(..)) ||"
+			+ "execution(* com.example.demo.controller.OrderController.*(..))")
+	public Object checkLogin(ProceedingJoinPoint jp) throws Throwable{
+		if(account == null || account.getUserName() == null || account.getUserName().length() == 0) {
+			System.err.println("ログインしていません！");
+			return "redirect:/";
+		}
+		
+		return jp.proceed();
+	}
+
+}
