cbf559245b1501276104470ed86892f197795e1b
Kawatan1927 <solxyz1927@gmail.com>
2024-05-15 11:32:50 +0900

    個人開発演習用プロジェクトを作成しました

---
+++ src/main/java/com/example/demo/PersonalDev6794KawataHarutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev6794KawataHarutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev6794KawataHarutoApplication.class, args);
+	}
+
+}
