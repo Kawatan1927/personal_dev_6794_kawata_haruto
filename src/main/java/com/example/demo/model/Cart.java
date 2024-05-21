be90aa9819e99ea5f9d4629f6e926deccec7a9cb
zougy <zougy@192.168.1.6>
2024-05-21 23:01:59 +0900

    [fix]使用したポイントが合計金額に反映されない不具合の修正

--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 import com.example.demo.entity.Item;
-import com.example.demo.entity.Timesale;
 import com.example.demo.repository.TimeSaleRepository;
 import com.example.demo.service.MakeTimesaleList;
 import com.example.demo.service.MakeTimesaleMapService;
@@ -18,94 +16,91 @@ import com.example.demo.service.MakeTimesaleMapService;
 @Component
 @SessionScope
 public class Cart {
-	
+
 	@Autowired
 	Account account;
-	
+
 	@Autowired
 	MakeTimesaleList makeTimesaleList;
-	
+
 	@Autowired
 	MakeTimesaleMapService makeTimesaleMapService;
-	
+
 	@Autowired
 	TimeSaleRepository timeSaleRepository;
-	
+
 	//商品リスト
 	private List<Item> itemList = new ArrayList<>();
 
-	
 	//商品リストゲッター
 	public List<Item> getItemList() {
 		return itemList;
 	}
-	
+
 	//合計金額取得用ゲッター
 	public Integer getTotalPrice() {
 		//セール情報の取得
 		List<Integer> timesaleItemList = makeTimesaleList.generate();
-		Map<Integer, Double> timesaleMap =  makeTimesaleMapService.generate();
+		Map<Integer, Double> timesaleMap = makeTimesaleMapService.generate();
 		//合計金額
 		int total = 0;
-		if(timesaleItemList.size() == 0) {
-			for(Item item : itemList) {
+		if (timesaleItemList.size() == 0) {
+			for (Item item : itemList) {
 				total += item.getPrice() * item.getQuantity() - account.getUsePoint();
 			}
-			
+			total -= account.getUsePoint();
 			return total;
-		}else {
+		} else {
 			int i = 0;
-			LocalDateTime now = LocalDateTime.now();
-			List<Timesale> timesaleList = timeSaleRepository.findByStartTimeLessThanEqualAndEndTimeGreaterThanEqual(now, now);
-			for(Item item : itemList) {
-				for(Integer saleId :timesaleItemList ) {
-					if(item.getId() == saleId) {
+			for (Item item : itemList) {
+				for (Integer saleId : timesaleItemList) {
+					if (item.getId() == saleId) {
 						total += item.getPrice() * timesaleMap.get(item.getId()) * item.getQuantity();
-						 i += 1;
+						i += 1;
 						break;
 					}
 				}
-				if(i == 0) {
-					total += item.getPrice()  * item.getQuantity();
+				if (i == 0) {
+					total += item.getPrice() * item.getQuantity();
 				}
-				
-			}return total;
+			}
+			total -= account.getUsePoint();
+			return total;
 		}
-		
+
 	}
-	
+
 	//獲得予定ポイント取得用ゲッター
 	public int getTotalPoint() {
 		int total = 0;
-		for(Item item : itemList) {
+		for (Item item : itemList) {
 			total += item.getPrice() * 0.1;
 		}
 		return total;
 	}
-	
+
 	//カート追加
 	public void add(Item newItem) {
 		account.setUsePoint(0);
 		Item existsItem = null;
 		//現在のカートの商品から同一IDの商品を探す
-		for(Item item : itemList) {
-			if(item.getId() == newItem.getId()) {
+		for (Item item : itemList) {
+			if (item.getId() == newItem.getId()) {
 				existsItem = item;
 				break;
 			}
 		}
-		
+
 		//カート内に用品が存在しなかった場合はカート追加
 		//存在した場合は個数の更新を行う
-		if(existsItem == null) {
+		if (existsItem == null) {
 			itemList.add(newItem);
-		}else {
-			existsItem.setQuantity(
-					existsItem.getQuantity() + newItem.getQuantity());
+		} else {
+			existsItem.setQuantity(existsItem.getQuantity() + newItem.getQuantity());
 		}
 	}
-	
-//	カートの個数を更新する
+
+	//	カートの個数を更新する
 	public void updateItems(Integer itemId, Integer quantity) {
 		Item existsItem = null;
 		//現在のカートの商品から同一のIDの商品を探す
@@ -115,34 +110,29 @@ public class Cart {
 				break;
 			}
 		}
-		if(existsItem != null) {
-		//個数の更新をセットする
-		existsItem.setQuantity(quantity);
+		if (existsItem != null) {
+			//個数の更新をセットする
+			existsItem.setQuantity(quantity);
 		}
 	}
 
-	
 	//カートから商品を削除
 	public void delete(int itemId) {
 		//現在のカートの商品から同一IDの商品を探す
-		for(Item item : itemList) {
-			if(item.getId() == itemId) {
+		for (Item item : itemList) {
+			if (item.getId() == itemId) {
 				itemList.remove(item);
 				break;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	//カートの中身を全てクリア
 	public void clear() {
 		itemList = new ArrayList<>();
-		
+
 	}
-	
-	
-	
-	
 
 }
